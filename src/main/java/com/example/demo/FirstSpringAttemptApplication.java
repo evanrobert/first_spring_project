fca9a6c116f98ab8850a74d71c761c9f19503b3c
Evan Lindsay <evanlindsay@Evans-Laptop.local>
2023-01-21 10:28:28 -0800

    first attempt at spring

---
+++ src/main/java/com/example/demo/FirstSpringAttemptApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAttemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAttemptApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/StatusService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+    import org.springframework.stereotype.Service;
+
+    @Service
+    public class StatusService {
+
+        public String processStatus(boolean arriving) {
+            if (arriving) {
+                return "/hello";
+            } else {
+                return "/goodbye";
+            }
+        }
+    }
+
